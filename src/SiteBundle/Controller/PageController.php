649f4d8df1905cc175e17e2a5f1e42429ac996f1
vstr91 <almir.amjunior@gmail.com>
2019-05-28 17:57:04 -0300

    Validação

--- src/SiteBundle/Controller/PageController.php
+++ src/SiteBundle/Controller/PageController.php
@@ -22,40 +22,102 @@ class PageController extends Controller {
     public function processaFormAction(\Symfony\Component\HttpFoundation\Request $request) {
 
         $entrada = new Entrada();
-        
+        $em = $this->getDoctrine()->getManager();
+
 //        $dataNascimento = $request->get('sitebundle_entrada')['dataNascimento'];
 //        
 //        $req = $request->request->all();
 //        $req['sitebundle_entrada']['dataNascimento'] = date_create_from_format('d/m/Y', $dataNascimento)->format('Y-m-d');
 //        
 //        $request->request->replace($req);
-        
+
         $form = $this->createForm(\SiteBundle\Form\EntradaType::class, $entrada, [
             'action' => '/processa-form'
         ]);
-        
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
             $entrada = $form->getData();
-            
+
+            // testa se CPF ja foi cadastrado
+            $ent = $em->getRepository('SiteBundle:Entrada')
+                    ->findOneBy(array('cpf' => $entrada->getCpf()));
+
+
+            if ($ent != null) {
+                
+            }
+
+            // fim teste CPF cadastrado
+
+            // valida CPF
+            if(!$this->validaCPF($entrada->getCpf())){
+                return new \Symfony\Component\HttpFoundation\Response('O CPF informado é inválido!');
+            }
+            // fim validacao CPF
+
+
             $entrada->setEscolhida(false);
-            
+
             // salva com quebra de linha
             $entrada->setFrase(nl2br(htmlentities($entrada->getFrase(), ENT_QUOTES, 'UTF-8')));
 
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-             $entityManager = $this->getDoctrine()->getManager();
-             $entityManager->persist($entrada);
-             $entityManager->flush();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($entrada);
+            $entityManager->flush();
 
-            return new \Symfony\Component\HttpFoundation\Response('ok');
+            return new \Symfony\Component\HttpFoundation\Response('0');
         }
 
-        return new \Symfony\Component\HttpFoundation\Response('erro');
+        return new \Symfony\Component\HttpFoundation\Response('Erro ao cadastrar frase... Por favor tente novamente.');
+    }
+
+    function validaCPF($cpf = null) {
+
+        // Verifica se um número foi informado
+        if (empty($cpf)) {
+            return false;
+        }
+
+        // Elimina possivel mascara
+        $cpf = preg_replace("/[^0-9]/", "", $cpf);
+        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+
+        // Verifica se o numero de digitos informados é igual a 11 
+        if (strlen($cpf) != 11) {
+            return false;
+        }
+        // Verifica se nenhuma das sequências invalidas abaixo 
+        // foi digitada. Caso afirmativo, retorna falso
+        else if ($cpf == '00000000000' ||
+                $cpf == '11111111111' ||
+                $cpf == '22222222222' ||
+                $cpf == '33333333333' ||
+                $cpf == '44444444444' ||
+                $cpf == '55555555555' ||
+                $cpf == '66666666666' ||
+                $cpf == '77777777777' ||
+                $cpf == '88888888888' ||
+                $cpf == '99999999999') {
+            return false;
+            // Calcula os digitos verificadores para verificar se o
+            // CPF é válido
+        } else {
+
+            for ($t = 9; $t < 11; $t++) {
+
+                for ($d = 0, $c = 0; $c < $t; $c++) {
+                    $d += $cpf{$c} * (($t + 1) - $c);
+                }
+                $d = ((10 * $d) % 11) % 10;
+                if ($cpf{$c} != $d) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 
 }
